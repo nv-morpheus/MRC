c3097205cf96ad1435949f36e8301cb97042caa9
Christopher Harris <xixonia@gmail.com>
2022-06-20 20:23:03 -0500

    .

--- src/internal/executor/iexecutor.cpp
+++ src/internal/executor/iexecutor.cpp
@@ -32,7 +32,8 @@ IExecutor::IExecutor(std::unique_ptr<Executor> impl) : m_impl(std::move(impl))
     CHECK(m_impl);
 }
 IExecutor::IExecutor(std::shared_ptr<Options> options) : IExecutor(make_executor(std::move(options))) {}
-IExecutor::IExecutor(std::unique_ptr<system::ISystem> system) : IExecutor(make_executor(system::System::unwrap(*system)))
+IExecutor::IExecutor(std::unique_ptr<system::ISystem> system) :
+  IExecutor(make_executor(system::System::unwrap(*system)))
 {}
 
 IExecutor::~IExecutor() = default;
