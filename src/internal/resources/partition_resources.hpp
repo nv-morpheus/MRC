56c584a8d72572d5bb49373c5fca679366f0c802
Ryan Olson <rolson@nvidia.com>
2022-06-25 05:56:13 +0000

    missing files

---
+++ src/internal/resources/partition_resources.hpp
@@ -0,0 +1,42 @@
+/**
+ * SPDX-FileCopyrightText: Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "internal/memory/resources.hpp"
+#include "internal/resources/partition_resources_base.hpp"
+#include "internal/runnable/resources.hpp"
+#include "internal/ucx/resources.hpp"
+
+#include "srf/memory/resources/memory_resource.hpp"
+
+#include <glog/logging.h>
+
+#include <optional>
+
+namespace srf::internal::resources {
+
+/**
+ * @brief Owner of most resources assigned to a specific partition.
+ */
+class PartitionResources final : public PartitionResourceBase
+{
+  public:
+    PartitionResources(runnable::Resources& runnable_resources, std::size_t partition_id);
+};
+
+}  // namespace srf::internal::resources
