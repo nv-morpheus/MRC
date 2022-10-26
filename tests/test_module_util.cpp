179186e9a1af530b3c7e499dd375830531223439
bsuryadevara <bhargavsuryadevara@gmail.com>
2022-10-26 13:22:38 -0500

    module registry util test

---
+++ tests/test_module_util.cpp
@@ -0,0 +1,39 @@
+/**
+ * SPDX-FileCopyrightText: Copyright (c) 2021-2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "srf/experimental/modules/segment_module_util.hpp"
+#include "srf/experimental/modules/sample_modules.hpp"
+
+#include "test_segment.hpp"
+
+TEST_F(SegmentTests, ModuleRegistryUtilTest)
+{
+    using namespace modules;
+
+    const auto *registry_namespace = "srf_unittest";
+
+    ModelRegistryUtil::register_module<SimpleModule>("SimpleModule", registry_namespace);
+    ModelRegistryUtil::register_module<SourceModule>("SourceModule", registry_namespace);
+    ModelRegistryUtil::register_module<SinkModule>("SinkModule", registry_namespace);
+    ModelRegistryUtil::register_module<NestedModule>("NestedModule", registry_namespace);
+    ModelRegistryUtil::register_module<ConfigurableModule>("ConfigurableModule", registry_namespace);
+    ModelRegistryUtil::register_module<TemplateModule<int>>("TemplateModuleInt", registry_namespace);
+    ModelRegistryUtil::register_module<TemplateModule<std::string>>("TemplateModuleString", registry_namespace);
+
+    EXPECT_THROW(ModelRegistryUtil::register_module<SimpleModule>("SimpleModule", registry_namespace),
+                                                                  std::invalid_argument);
+}
