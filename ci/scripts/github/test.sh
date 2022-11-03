8459f2d974ee5bd6813e10a365e89c5a702dc768
David Gardner <dagardner@nvidia.com>
2022-11-03 10:15:22 -0700

    Remove out of date comment

--- ci/scripts/github/test.sh
+++ ci/scripts/github/test.sh
@@ -48,7 +48,6 @@ cd ${SRF_ROOT}/build
 set +e
 # Tests known to be failing
 # Issues:
-# * test_srf_benchmarking - https://github.com/nv-morpheus/SRF/issues/32
 # * test_srf_private - https://github.com/nv-morpheus/SRF/issues/33
 # * nvrpc - https://github.com/nv-morpheus/SRF/issues/34
 ctest --output-on-failure \
