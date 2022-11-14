88fd96e96d71be5a80b2b496871a7636df48d170
Devin Robison <drobison@nvidia.com>
2022-11-14 14:49:34 -0700

    Clang tidy fix

--- tests/modules/test_modules.hpp
+++ tests/modules/test_modules.hpp
@@ -67,7 +67,7 @@ class TestModules : public ::testing::Test
     std::shared_ptr<TestSegmentResources> m_resources;
 };
 
-using TestMirrorTapModule = TestModules; // NOLINT
-using TestModuleRegistry  = TestModules; // NOLINT
-using TestModuleUtil      = TestModules; // NOLINT
-using TestSegmentModules  = TestModules; // NOLINT
+using TestMirrorTapModule = TestModules;  // NOLINT
+using TestModuleRegistry  = TestModules;  // NOLINT
+using TestModuleUtil      = TestModules;  // NOLINT
+using TestSegmentModules  = TestModules;  // NOLINT
