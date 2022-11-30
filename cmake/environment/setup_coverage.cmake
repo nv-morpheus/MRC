2ee3dca059cfbcfc7538f246851dc3575e1c73a1
Devin Robison <drobison@nvidia.com>
2022-11-29 17:03:10 -0700

    Path changes

--- cmake/environment/setup_coverage.cmake
+++ cmake/environment/setup_coverage.cmake
@@ -22,7 +22,7 @@ list(APPEND CMAKE_MESSAGE_CONTEXT "coverage")
 
 # Include coverage tools if enabled
 if(MRC_ENABLE_CODECOV)
-  include(${MORPHEUS_UTILS_HOME}/cmake/environment_config/gcov/setup.cmake)
+  include(${MORPHEUS_UTILS_HOME}/cmake/source/utils/code_coverage/setup.cmake)
 
   message(STATUS "MRC_ENABLE_CODECOV is ON, configuring report exclusions and setting up coverage build targets")
   set(CODECOV_REPORT_EXCLUSIONS
