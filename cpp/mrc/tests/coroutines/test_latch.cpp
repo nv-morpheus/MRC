3c1568322e1549a350e4e23dacfea3d9430e415b
Ryan Olson <ryanolson@users.noreply.github.com>
2022-12-09 15:39:24 -0700

    C++20 Coroutines (#244)
    Implementation based on the work of jbaldwin/libcoro.
    
    The reason for the fork is due to the fact that we will need to touch every awaitable object to apply a MRC context. The other major difference is ThreadPool/Scheduler/Executor discovery and a new SchedulingPolicy that will enable us to replicate boost::fiber behaviors in some critical places where coroutines will eventually replace fibers.
    
    Blocked by #239
    
    Authors:
      - Ryan Olson (https://github.com/ryanolson)
    
    Approvers:
      - Michael Demoret (https://github.com/mdemoret-nv)
    
    URL: https://github.com/nv-morpheus/MRC/pull/244

---
+++ cpp/mrc/tests/coroutines/test_latch.cpp
@@ -0,0 +1,158 @@
+/**
+ * SPDX-FileCopyrightText: Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Original Source: https://github.com/jbaldwin/libcoro
+ * Original License: Apache License, Version 2.0; included below
+ */
+
+/**
+ * Copyright 2021 Josh Baldwin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "mrc/coroutines/event.hpp"
+#include "mrc/coroutines/latch.hpp"
+#include "mrc/coroutines/task.hpp"
+
+#include <gtest/gtest.h>
+
+#include <chrono>
+#include <thread>
+
+using namespace mrc;
+
+class TestCoroLatch : public ::testing::Test
+{};
+
+TEST_F(TestCoroLatch, Count0)
+{
+    coroutines::Latch l{0};
+
+    auto make_task = [&]() -> coroutines::Task<uint64_t> {
+        co_await l;
+        co_return 42;
+    };
+
+    auto task = make_task();
+
+    task.resume();
+
+    EXPECT_TRUE(task.is_ready());
+    EXPECT_EQ(task.promise().result(), 42);
+}
+
+TEST_F(TestCoroLatch, Count1)
+{
+    coroutines::Latch l{1};
+
+    auto make_task = [&]() -> coroutines::Task<uint64_t> {
+        auto workers = l.remaining();
+        co_await l;
+        co_return workers;
+    };
+
+    auto task = make_task();
+
+    task.resume();
+    EXPECT_FALSE(task.is_ready());
+
+    l.count_down();
+    EXPECT_TRUE(task.is_ready());
+    EXPECT_EQ(task.promise().result(), 1);
+}
+
+TEST_F(TestCoroLatch, Count1Down5)
+{
+    coroutines::Latch l{1};
+
+    auto make_task = [&]() -> coroutines::Task<uint64_t> {
+        auto workers = l.remaining();
+        co_await l;
+        co_return workers;
+    };
+
+    auto task = make_task();
+
+    task.resume();
+    EXPECT_FALSE(task.is_ready());
+
+    l.count_down(5);
+    EXPECT_TRUE(task.is_ready());
+    EXPECT_TRUE(task.promise().result() == 1);
+}
+
+// TEST_CASE("latch count=5 count_down=1 x5", "[latch]")
+TEST_F(TestCoroLatch, Count5Down1x5)
+{
+    coroutines::Latch l{5};
+
+    auto make_task = [&]() -> coroutines::Task<uint64_t> {
+        auto workers = l.remaining();
+        co_await l;
+        co_return workers;
+    };
+
+    auto task = make_task();
+
+    task.resume();
+    EXPECT_FALSE(task.is_ready());
+
+    l.count_down(1);
+    EXPECT_FALSE(task.is_ready());
+    l.count_down(1);
+    EXPECT_FALSE(task.is_ready());
+    l.count_down(1);
+    EXPECT_FALSE(task.is_ready());
+    l.count_down(1);
+    EXPECT_FALSE(task.is_ready());
+    l.count_down(1);
+    EXPECT_TRUE(task.is_ready());
+    EXPECT_TRUE(task.promise().result() == 5);
+}
+
+// TEST_CASE("latch count=5 count_down=5", "[latch]")
+TEST_F(TestCoroLatch, Count5Down5)
+{
+    coroutines::Latch l{5};
+
+    auto make_task = [&]() -> coroutines::Task<uint64_t> {
+        auto workers = l.remaining();
+        co_await l;
+        co_return workers;
+    };
+
+    auto task = make_task();
+
+    task.resume();
+    EXPECT_FALSE(task.is_ready());
+
+    l.count_down(5);
+    EXPECT_TRUE(task.is_ready());
+    EXPECT_TRUE(task.promise().result() == 5);
+}
