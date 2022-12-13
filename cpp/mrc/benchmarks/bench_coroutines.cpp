dd12f09180da8ff8ce8fa48e43be4bc7b1fa2d6b
Ryan Olson <rolson@nvidia.com>
2022-12-13 05:37:38 +0000

    adding first awaitable benchmarks

--- cpp/mrc/benchmarks/bench_coroutines.cpp
+++ cpp/mrc/benchmarks/bench_coroutines.cpp
@@ -20,6 +20,8 @@
 
 #include <benchmark/benchmark.h>
 
+#include <coroutine>
+
 using namespace mrc;
 
 static void mrc_coro_create_single_task_and_sync(benchmark::State& state)
@@ -52,6 +54,83 @@ static void mrc_coro_create_two_tasks_and_sync_on_when_all(benchmark::State& sta
     }
 }
 
+static void mrc_coro_await_suspend_never(benchmark::State& state)
+{
+    auto task = [&]() -> coroutines::Task<void> {
+        for (auto _ : state)
+        {
+            co_await std::suspend_never{};
+        }
+        co_return;
+    };
+
+    coroutines::sync_wait(task());
+}
+
+// not-thread safe awaitable that returns a value
+class IncrementingAwaitable
+{
+    std::size_t m_counter{0};
+
+    struct Awaiter
+    {
+        constexpr Awaiter(IncrementingAwaitable& parent) : m_parent(parent) {}
+
+        constexpr static std::true_type await_ready() noexcept
+        {
+            return {};
+        }
+
+        constexpr static void await_suspend(std::coroutine_handle<> handle){};
+
+        std::size_t await_resume() noexcept
+        {
+            return ++(m_parent.m_counter);
+        }
+
+        IncrementingAwaitable& m_parent;
+    };
+
+  public:
+    [[nodiscard]] Awaiter operator co_await() noexcept
+    {
+        return {*this};
+    }
+};
+
+static void mrc_coro_await_incrementing_awaitable(benchmark::State& state)
+{
+    IncrementingAwaitable awaitable;
+    auto task = [&]() -> coroutines::Task<void> {
+        std::size_t i;
+        for (auto _ : state)
+        {
+            benchmark::DoNotOptimize(i = co_await awaitable);
+        }
+        co_return;
+    };
+
+    coroutines::sync_wait(task());
+}
+
+static void mrc_coro_await_incrementing_awaitable_baseline(benchmark::State& state)
+{
+    auto task = [&]() -> coroutines::Task<void> {
+        std::size_t i{0};
+        std::size_t j{0};
+        for (auto _ : state)
+        {
+            benchmark::DoNotOptimize(i = ++j);
+        }
+        co_return;
+    };
+
+    coroutines::sync_wait(task());
+}
+
 BENCHMARK(mrc_coro_create_single_task_and_sync);
 BENCHMARK(mrc_coro_create_single_task_and_sync_on_when_all);
 BENCHMARK(mrc_coro_create_two_tasks_and_sync_on_when_all);
+BENCHMARK(mrc_coro_await_suspend_never);
+BENCHMARK(mrc_coro_await_incrementing_awaitable_baseline);
+BENCHMARK(mrc_coro_await_incrementing_awaitable);
