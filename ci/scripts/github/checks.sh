ab41b44cfa6dfad4971c356d4a5c55df778c924f
Devin Robison <drobison@nvidia.com>
2023-01-09 11:12:37 -0700

    CI testing

--- ci/scripts/github/checks.sh
+++ ci/scripts/github/checks.sh
@@ -23,6 +23,8 @@ fetch_base_branch
 update_conda_env
 
 rapids-logger "Configuring CMake"
+git submodule init
+git submodule update
 cmake -B build -G Ninja ${CMAKE_BUILD_ALL_FEATURES} .
 
 rapids-logger "Building targets that generate source code"
