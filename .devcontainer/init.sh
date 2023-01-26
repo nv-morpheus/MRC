6891375dc1a7981d3957d10292cb8083e8c01c4f
Christopher Harris <xixonia@gmail.com>
2023-01-26 15:38:25 +0000

    .

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -14,8 +14,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# create a docker network for morpheus
-docker network inspect morpheus >/dev/null 2>&1 || docker network create morpheus
-
-# create the parent conda folder so it's found when mounting
+# create the github config directory if it does not exist so it's found when mounting
 mkdir -p ~/.config/gh
