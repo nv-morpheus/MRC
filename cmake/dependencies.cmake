9577d64f9ba5514b0e08b3677cc6577e7b303ccd
Michael Demoret <mdemoret@nvidia.com>
2023-01-27 06:36:18 +0000

    Updating utilities for updated boost

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -32,7 +32,7 @@ rapids_find_package(CUDAToolkit
 # =====
 # - Use static linking to avoid issues with system-wide installations of Boost.
 # - Use numa=on to ensure the numa component of fiber gets built
-morpheus_utils_configure_boost_boost_cmake()
+morpheus_utils_configure_boost()
 
 # UCX
 # ===
