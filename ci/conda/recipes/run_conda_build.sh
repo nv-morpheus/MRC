b207fd6af8100f9aafcee12641174a2157d1d291
Christopher Harris <xixonia@gmail.com>
2023-04-14 12:26:36 -0500

    Fix nullptr dereference in NvmlState (#319)
    Fixes a bug where `NvmlState` was dereferencing a `nullptr` when `libnvidia-ml.so.1` is not available.
    
    Authors:
      - Christopher Harris (https://github.com/cwharris)
    
    Approvers:
      - Devin Robison (https://github.com/drobison00)
    
    URL: https://github.com/nv-morpheus/MRC/pull/319

--- ci/conda/recipes/run_conda_build.sh
+++ ci/conda/recipes/run_conda_build.sh
@@ -99,7 +99,7 @@ fi
 # Choose default variants
 if hasArg quick; then
    # For quick build, just do most recent version of rapids
-   CONDA_ARGS_ARRAY+=("rapids_version: 23.02}")
+   CONDA_ARGS_ARRAY+=("--variants" "{rapids_version: 23.02}")
 fi
 
 # And default channels
