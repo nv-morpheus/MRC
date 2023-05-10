59a9474ccca14947f600ff5be3b07441682897ad
David Gardner <96306125+dagardner-nv@users.noreply.github.com>
2023-05-09 17:44:27 -0700

    RxNodeComponent should set exceptions on the context (#326)
    This allows any thrown exceptions to be handled by the runner.
    
    Authors:
      - David Gardner (https://github.com/dagardner-nv)
    
    Approvers:
      - Michael Demoret (https://github.com/mdemoret-nv)
    
    URL: https://github.com/nv-morpheus/MRC/pull/326

--- cpp/mrc/include/mrc/node/rx_node.hpp
+++ cpp/mrc/include/mrc/node/rx_node.hpp
@@ -234,6 +234,9 @@ class RxNodeComponent : public WritableProvider<InputT>, public WritableAcceptor
                 // Forward to the writable edge
                 this->get_writable_edge()->await_write(std::move(message));
             },
+            [this](std::exception_ptr ptr) {
+                runnable::Context::get_runtime_context().set_exception(std::move(ptr));
+            },
             [this]() {
                 // On completion, release connections
                 WritableAcceptor<OutputT>::release_edge_connection();
