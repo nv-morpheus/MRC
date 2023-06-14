7477fc934e19861f464cbd82bd2ad00961544d22
Devin Robison <drobison@nvidia.com>
2023-06-14 15:47:00 -0600

    Fix noqa line

--- python/tests/test_segment_modules.py
+++ python/tests/test_segment_modules.py
@@ -369,7 +369,7 @@ def test_py_modules_dont_overwrite():
         # Make sure we can't re-register the same name
         with pytest.raises(RuntimeError):
             this_should_fail = builder.load_module("NestedModule", "mrc_unittest", "ModuleNestingTest_mod1",
-                                                   {})  # NOLINT
+                                                   {})  # noqa: E841
 
         nested_mod2 = builder.load_module("NestedModule", "mrc_unittest", "ModuleNestingTest_mod2", {})
         nested_sink = builder.make_sink("nested_sink", on_next, on_error, on_complete)
