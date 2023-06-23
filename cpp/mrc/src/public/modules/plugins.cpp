fea298ff6bf13ff7d8940515c964b082202803ac
Michael Demoret <42954918+mdemoret-nv@users.noreply.github.com>
2023-06-23 13:34:48 -0400

    Remove `boost::filesystem` (#334)
    This PR removes all references to `boost::filesystem` in favor of `std::filesystem`. The std library should be preferred over the boost version
    
    Authors:
      - Michael Demoret (https://github.com/mdemoret-nv)
    
    Approvers:
      - Christopher Harris (https://github.com/cwharris)
    
    URL: https://github.com/nv-morpheus/MRC/pull/334

--- cpp/mrc/src/public/modules/plugins.cpp
+++ cpp/mrc/src/public/modules/plugins.cpp
@@ -17,13 +17,12 @@
 
 #include "mrc/modules/plugins.hpp"
 
-#include <boost/filesystem/operations.hpp>
-#include <boost/filesystem/path.hpp>
-#include <boost/filesystem/path_traits.hpp>
 #include <dlfcn.h>
 #include <glog/logging.h>
 
+#include <chrono>
 #include <exception>
+#include <filesystem>
 #include <memory>
 #include <mutex>
 #include <sstream>
@@ -31,7 +30,7 @@
 #include <string>
 #include <utility>
 
-namespace fs = boost::filesystem;
+namespace fs = std::filesystem;
 
 namespace mrc::modules {
 
