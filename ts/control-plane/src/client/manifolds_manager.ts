8cb524d385a611868e9802470cd343e8feea99c7
David Gardner <dagardner@nvidia.com>
2023-06-30 15:27:31 -0700

    Add methods for setting the Actual status for a manifold

--- ts/control-plane/src/client/manifolds_manager.ts
+++ ts/control-plane/src/client/manifolds_manager.ts
@@ -4,15 +4,20 @@ import {
    ManifoldUpdateActualAssignmentsRequest,
 } from "@mrc/proto/mrc/protos/architect";
 import { PipelineManager } from "./pipeline_manager";
+import { ResourceActualStatus, ResourceRequestedStatus, resourceActualStatusToNumber, resourceRequestedStatusToNumber } from "@mrc/proto/mrc/protos/architect_state";
 
 export class ManifoldClientInstance {
    constructor(public readonly manifoldId: string, private readonly manifoldsManager: ManifoldsManager) {}
+
+   get connectionManager() {
+      return this.manifoldsManager.connectionManager;
+   }
 
    public async syncActualSegments() {
       const manifoldState = this.getState();
 
       const response =
-         await this.manifoldsManager.connectionManager.send_request<ManifoldUpdateActualAssignmentsResponse>(
+         await this.connectionManager.send_request<ManifoldUpdateActualAssignmentsResponse>(
             EventType.ClientUnaryManifoldUpdateActualAssignments,
             ManifoldUpdateActualAssignmentsRequest.create({
                manifoldInstanceId: this.manifoldId,
@@ -24,6 +29,36 @@ export class ManifoldClientInstance {
       if (!response.ok) {
          throw new Error("Failed to sync actual segments");
       }
+   }
+
+   public async syncActualStatus() {
+      const segmentState = this.getState();
+      const reqStatus = segmentState.state!.requestedStatus;
+      const currentActualStatus = segmentState.state!.actualStatus;
+      if (resourceActualStatusToNumber(currentActualStatus) > resourceRequestedStatusToNumber(reqStatus)) {
+         throw new Error(`Invalid state: actual status ${currentActualStatus} is greater than requested status ${reqStatus}`);
+      }
+
+      let actualStatus: ResourceActualStatus;
+      switch (reqStatus) {
+         case ResourceRequestedStatus.Requested_Completed:
+            actualStatus = ResourceActualStatus.Actual_Running;
+            break;
+         case ResourceRequestedStatus.Requested_Stopped:
+            actualStatus = ResourceActualStatus.Actual_Stopping;
+            break;
+         case ResourceRequestedStatus.Requested_Destroyed:
+            actualStatus = ResourceActualStatus.Actual_Destroying;
+            break;
+         default:
+            throw new Error(`Invalid state: requested status ${reqStatus} is not a valid requested status`);
+      }
+
+      return await this.updateActualStatus(actualStatus);
+   }
+
+   public async updateActualStatus(status: ResourceActualStatus) {
+      return await this.connectionManager.update_resource_status(this.manifoldId, "ManifoldInstances", status);
    }
 
    public getState() {
