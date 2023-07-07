dc7c086d614af93639cc1012ea03f1e0e0c1b485
Michael Demoret <mdemoret@nvidia.com>
2023-07-07 11:39:02 -0600

    Adding option to configure running the conda-build CI step with labels

--- ci/scripts/github/conda.sh
+++ ci/scripts/github/conda.sh
@@ -39,4 +39,4 @@ conda info
 rapids-logger "Building Conda Package"
 
 # Run the conda build and upload
-${MRC_ROOT}/ci/conda/recipes/run_conda_build.sh upload
+${MRC_ROOT}/ci/conda/recipes/run_conda_build.sh "$@"
