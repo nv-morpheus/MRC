ef069721706595442dcf34d38b61acac0bd885db
David Gardner <dagardner@nvidia.com>
2023-08-02 10:54:44 -0700

    Don't skip this test as its perfectly valid and passes, the referenced issue isn't relevent to this test

--- cpp/mrc/tests/test_pipeline.cpp
+++ cpp/mrc/tests/test_pipeline.cpp
@@ -105,8 +105,6 @@ TEST_F(TestPipeline, DuplicateSegments)
 
 TEST_F(TestPipeline, TwoSegment)
 {
-    GTEST_SKIP() << "#185";
-
     std::atomic<int> next_count     = 0;
     std::atomic<int> complete_count = 0;
 
