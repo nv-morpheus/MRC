2714b04f1db2e32a6cf7a926152bac9b66a94e41
David Gardner <dagardner@nvidia.com>
2023-08-18 09:48:31 -0700

    I can get ~ObjUsingGil() to be triggered by ObjCallingGC calling gc.collect

--- python/tests/test_gil_tls.py
+++ python/tests/test_gil_tls.py
@@ -1,42 +1,49 @@
 import gc
 import threading
-import time
 import weakref
 
 from mrc.tests.utils import ObjCallingGC
 from mrc.tests.utils import ObjUsingGil
 
 
+class Holder:
+
+    def __init__(self, obj):
+        """Intentionally create a cycle to delay obj's destruction"""
+        self.obj = obj
+        self.cycle = self
+
+    def __del__(self):
+        print("Holder.__del__", flush=True)
+        self.obj = None
+
+
 class ThreadTest(threading.Thread):
 
     def _create_obs(self):
-        ocg = ObjCallingGC()
-        oug = ObjUsingGil()
-
-        weakref.finalize(ocg, ocg.finalize)
-        tls = threading.local()
-        tls.oug = oug
-        tls.ogc = ocg
+        self.h = Holder(ObjUsingGil())
+        self.ocg = ObjCallingGC()
+        weakref.finalize(self.ocg, self.ocg.finalize)
 
     def run(self):
         print("Running thread", flush=True)
         self._create_obs()
-
-        print("Sleeping", flush=True)
-        time.sleep(1)
-        print("Done sleeping", flush=True)
+        print("Thread complete", flush=True)
 
 
 def test_gil_tls():
     t = ThreadTest()
     t.start()
     t.join()
+    print("Thread joined, dereferencing thread", flush=True)
+    t = None
 
 
 def main():
     gc.disable()
     gc.set_debug(gc.DEBUG_STATS)
     test_gil_tls()
+    print("Exiting main", flush=True)
 
 
 if __name__ == "__main__":
