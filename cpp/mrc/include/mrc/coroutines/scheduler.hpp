62e18346972fea8cfa25e7d72976a9f37da71ada
Christopher Harris <xixonia@gmail.com>
2023-11-02 17:33:43 -0500

    Add AsyncioRunnable (#411)
    Moves the CoroutineRunnable from Morpheus' Sherlock feature branch to MRC and renames it to AsyncioRunnable as it is heavily dependent on asyncio. Adjustments were made such that the Scheduler would no longer own a task container and/or tasks, leaving the scheduler interface simpler. Instead, the runnable is responsible for the lifetime of the tasks it creates. This leaves the scheduler with a single responsibility.
    
    Much of the code could be moved to MRC proper from PyMRC, but it's not immediately obvious where the code should live or whether it would be reused, so keeping it colocated with the AsyncioRunnable makes the most sense for now, imo.
    
    Authors:
      - Christopher Harris (https://github.com/cwharris)
    
    Approvers:
      - Devin Robison (https://github.com/drobison00)
    
    URL: https://github.com/nv-morpheus/MRC/pull/411

--- cpp/mrc/include/mrc/coroutines/scheduler.hpp
+++ cpp/mrc/include/mrc/coroutines/scheduler.hpp
@@ -25,109 +25,30 @@
 #include <mutex>
 #include <string>
 
-// IWYU thinks this is needed, but it's not
-// IWYU pragma: no_include "mrc/coroutines/task_container.hpp"
-
 namespace mrc::coroutines {
-
-class TaskContainer;  // IWYU pragma: keep
 
 /**
  * @brief Scheduler base class
- *
- * Allows all schedulers to be discovered via the mrc::this_thread::current_scheduler()
  */
 class Scheduler : public std::enable_shared_from_this<Scheduler>
 {
   public:
-    struct Operation
-    {
-        Operation(Scheduler& scheduler);
-
-        constexpr static auto await_ready() noexcept -> bool
-        {
-            return false;
-        }
-
-        std::coroutine_handle<> await_suspend(std::coroutine_handle<> awaiting_coroutine) noexcept;
-
-        constexpr static auto await_resume() noexcept -> void {}
-
-        Scheduler& m_scheduler;
-        std::coroutine_handle<> m_awaiting_coroutine;
-        Operation* m_next{nullptr};
-    };
-
-    Scheduler();
     virtual ~Scheduler() = default;
 
     /**
-     * @brief Description of Scheduler
+     * @brief Resumes a coroutine according to the scheduler's implementation.
      */
-    virtual std::string description() const = 0;
+    virtual void resume(std::coroutine_handle<> handle) noexcept = 0;
 
     /**
-     * Schedules the currently executing coroutine to be run on this thread pool.  This must be
-     * called from within the coroutines function body to schedule the coroutine on the thread pool.
-     * @throw std::runtime_error If the thread pool is `shutdown()` scheduling new tasks is not permitted.
-     * @return The operation to switch from the calling scheduling thread to the executor thread
-     *         pool thread.
+     * @brief Suspends the current function and resumes it according to the scheduler's implementation.
      */
-    [[nodiscard]] virtual auto schedule() -> Operation;
-
-    // Enqueues a message without waiting for it. Must return void since the caller will not get the return value
-    virtual void schedule(Task<void>&& task);
+    [[nodiscard]] virtual Task<> schedule() = 0;
 
     /**
-     * Schedules any coroutine handle that is ready to be resumed.
-     * @param handle The coroutine handle to schedule.
+     * @brief Suspends the current function and resumes it according to the scheduler's implementation.
      */
-    virtual auto resume(std::coroutine_handle<> coroutine) -> void = 0;
-
-    /**
-     * Yields the current task to the end of the queue of waiting tasks.
-     */
-    [[nodiscard]] auto yield() -> Operation;
-
-    /**
-     * If the calling thread controlled by a Scheduler, return a pointer to the Scheduler
-     */
-    static auto from_current_thread() noexcept -> Scheduler*;
-
-    /**
-     * If the calling thread is owned by a thread_pool, return the thread index (rank) of the current thread with
-     * respect the threads in the pool; otherwise, return the std::hash of std::this_thread::get_id
-     */
-    static auto get_thread_id() noexcept -> std::size_t;
-
-  protected:
-    virtual auto on_thread_start(std::size_t) -> void;
-
-    /**
-     * @brief Get the task container object
-     *
-     * @return TaskContainer&
-     */
-    TaskContainer& get_task_container() const;
-
-  private:
-    /**
-     * @brief When co_await schedule() is called, this function will be executed by the awaiter. Each scheduler
-     * implementation should determine how and when to execute the operation.
-     *
-     * @param operation The schedule() awaitable pointer
-     * @return std::coroutine_handle<> Return a coroutine handle to which will be
-     * used as the return value for await_suspend().
-     */
-    virtual std::coroutine_handle<> schedule_operation(Operation* operation) = 0;
-
-    mutable std::mutex m_mutex;
-
-    // Maintains the lifetime of fire-and-forget tasks scheduled with schedule(Task<void>&& task)
-    std::unique_ptr<TaskContainer> m_task_container;
-
-    thread_local static Scheduler* m_thread_local_scheduler;
-    thread_local static std::size_t m_thread_id;
+    [[nodiscard]] virtual Task<> yield() = 0;
 };
 
 }  // namespace mrc::coroutines
---
+++ python/mrc/_pymrc/include/pymrc/asyncio_scheduler.hpp
@@ -0,0 +1,105 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "pymrc/coro.hpp"
+#include "pymrc/utilities/acquire_gil.hpp"
+#include "pymrc/utilities/object_wrappers.hpp"
+
+#include <boost/fiber/future/async.hpp>
+#include <mrc/coroutines/scheduler.hpp>
+#include <mrc/coroutines/task.hpp>
+#include <mrc/coroutines/task_container.hpp>
+#include <pybind11/pybind11.h>
+#include <pybind11/pytypes.h>
+
+#include <stdexcept>
+#include <utility>
+
+namespace py = pybind11;
+
+namespace mrc::pymrc {
+
+/**
+ * @brief A MRC Scheduler which allows resuming C++20 coroutines on an Asyncio event loop.
+ */
+class AsyncioScheduler : public mrc::coroutines::Scheduler
+{
+  private:
+    class ContinueOnLoopOperation
+    {
+      public:
+        ContinueOnLoopOperation(PyObjectHolder loop) : m_loop(std::move(loop)) {}
+
+        static bool await_ready() noexcept
+        {
+            return false;
+        }
+
+        void await_suspend(std::coroutine_handle<> handle) noexcept
+        {
+            AsyncioScheduler::resume(m_loop, handle);
+        }
+
+        static void await_resume() noexcept {}
+
+      private:
+        PyObjectHolder m_loop;
+    };
+
+    static void resume(PyObjectHolder loop, std::coroutine_handle<> handle) noexcept
+    {
+        pybind11::gil_scoped_acquire acquire;
+        loop.attr("call_soon_threadsafe")(pybind11::cpp_function([handle]() {
+            pybind11::gil_scoped_release release;
+            handle.resume();
+        }));
+    }
+
+  public:
+    AsyncioScheduler(PyObjectHolder loop) : m_loop(std::move(loop)) {}
+
+    /**
+     * @brief Resumes a coroutine on the scheduler's Asyncio event loop
+     */
+    void resume(std::coroutine_handle<> handle) noexcept override
+    {
+        AsyncioScheduler::resume(m_loop, handle);
+    }
+
+    /**
+     * @brief Suspends the current function and resumes it on the scheduler's Asyncio event loop
+     */
+    [[nodiscard]] coroutines::Task<> schedule() override
+    {
+        co_await ContinueOnLoopOperation(m_loop);
+    }
+
+    /**
+     * @brief Suspends the current function and resumes it on the scheduler's Asyncio event loop
+     */
+    [[nodiscard]] coroutines::Task<> yield() override
+    {
+        co_await ContinueOnLoopOperation(m_loop);
+    }
+
+  private:
+    mrc::pymrc::PyHolder m_loop;
+};
+
+}  // namespace mrc::pymrc
