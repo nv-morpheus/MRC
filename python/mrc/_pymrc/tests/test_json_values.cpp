b2758acbe0b02d84dfa11eed6a66ba29756ecaa3
David Gardner <dagardner@nvidia.com>
2024-03-13 15:18:49 -0700

    Test with more unserializable types

--- python/mrc/_pymrc/tests/test_json_values.cpp
+++ python/mrc/_pymrc/tests/test_json_values.cpp
@@ -20,13 +20,17 @@
 #include "pymrc/utilities/json_values.hpp"
 
 #include <gtest/gtest.h>
-#include <pybind11/cast.h>
 #include <pybind11/pybind11.h>
-#include <pybind11/pytypes.h>
 #include <pybind11/stl.h>  // IWYU pragma: keep
 
+#include <cstddef>  // for size_t
 #include <string>
+#include <utility>  // for pair
 #include <vector>
+// We already included pybind11.h don't need these others
+// IWYU pragma: no_include <pybind11/cast.h>
+// IWYU pragma: no_include <pybind11/eval.h>
+// IWYU pragma: no_include <pybind11/pytypes.h>
 
 namespace py = pybind11;
 using namespace mrc::pymrc;
@@ -121,4 +125,60 @@ TEST_F(TestJSONValues, ToPythonList)
     EXPECT_TRUE(result.equal(py_list));
     py::object result_dec = result[2];
     EXPECT_TRUE(result_dec.is(py_dec));
+}
+
+TEST_F(TestJSONValues, ToPythonMultipleTypes)
+{
+    // Test with miultiple types not json serializable: module, class, function, generator
+    py::object py_mod  = py::module_::import("decimal");
+    py::object py_cls  = py_mod.attr("Decimal");
+    py::object globals = py::globals();
+    py::exec(
+        R"(
+            def gen_fn():
+                yield 1
+        )",
+        globals);
+
+    py::object py_fn  = globals["gen_fn"];
+    py::object py_gen = py_fn();
+
+    std::vector<std::pair<std::size_t, py::object>> expected_list_objs = {{1, py_mod},
+                                                                          {3, py_cls},
+                                                                          {5, py_fn},
+                                                                          {7, py_gen}};
+
+    std::vector<py::object> py_values =
+        {py::cast(0), py_mod, py::cast(2), py_cls, py::cast(4), py_fn, py::cast(6), py_gen};
+    py::list py_list = py::cast(py_values);
+
+    std::vector<std::pair<std::string, py::object>> expected_dict_objs = {{"module", py_mod},
+                                                                          {"class", py_cls},
+                                                                          {"function", py_fn},
+                                                                          {"generator", py_gen}};
+
+    // Test with object in a nested dict
+    py::dict py_dict("module"_a      = py_mod,
+                     "class"_a       = py_cls,
+                     "function"_a    = py_fn,
+                     "generator"_a   = py_gen,
+                     "nested_list"_a = py_list);
+
+    JSONValues j{py_dict};
+    auto result = j.to_python();
+    EXPECT_TRUE(result.equal(py_dict));
+    EXPECT_FALSE(result.is(py_dict));  // Ensure we actually serialized the object and not stored it
+
+    for (const auto& [key, value] : expected_dict_objs)
+    {
+        py::object result_value = result[key.c_str()];
+        EXPECT_TRUE(result_value.is(value));
+    }
+
+    py::list nested_list = result["nested_list"];
+    for (const auto& [index, value] : expected_list_objs)
+    {
+        py::object result_value = nested_list[index];
+        EXPECT_TRUE(result_value.is(value));
+    }
 }
