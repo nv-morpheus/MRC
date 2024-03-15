6b1bc990635a74029e51f816ee32033b70ef2662
Christopher Harris <xixonia@gmail.com>
2024-03-15 19:25:19 +0000

    add comments to TestScheduler

--- cpp/mrc/include/mrc/coroutines/test_scheduler.hpp
+++ cpp/mrc/include/mrc/coroutines/test_scheduler.hpp
@@ -40,18 +40,46 @@ class TestScheduler : public Scheduler
     std::chrono::time_point<std::chrono::steady_clock> m_time = std::chrono::steady_clock::now();
 
   public:
+
+    /**
+     * @brief Enqueue's the coroutine handle to be resumed at the current logical time.
+     */
     void resume(std::coroutine_handle<> handle) noexcept override;
 
+    /**
+     * Suspends the current function and enqueue's it to be resumed at the current logical time.
+     */
     mrc::coroutines::Task<> yield() override;
 
+    /**
+     * Suspends the current function and enqueue's it to be resumed at the current logica time + the given duration.
+     */
     mrc::coroutines::Task<> yield_for(std::chrono::milliseconds time) override;
 
+    /**
+     * Suspends the current function and enqueue's it to be resumed at the given logical time.
+     */
     mrc::coroutines::Task<> yield_until(std::chrono::time_point<std::chrono::steady_clock> time) override;
 
+    /**
+     * Immediately resumes the next-in-queue coroutine handle.
+     *
+     *  @return true if more coroutines exist in the queue after resuming, false otherwise.
+     */
     bool resume_next();
 
+    /**
+     * Immediately resumes next-in-queue coroutines up to the current logical time + the given duration, in-order.
+     *
+     *  @return true if more coroutines exist in the queue after resuming, false otherwise.
+     */
     bool resume_for(std::chrono::milliseconds time);
 
+    /**
+     * Immediately resumes next-in-queue coroutines up to the given logical time.
+     *
+     *  @return true if more coroutines exist in the queue after resuming, false otherwise.
+    */
     bool resume_until(std::chrono::time_point<std::chrono::steady_clock> time);
 };
 
