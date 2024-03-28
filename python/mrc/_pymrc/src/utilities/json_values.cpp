c880e64dbdca18e8a38c466a9f5bbfe12219ffe6
David Gardner <dagardner@nvidia.com>
2024-03-28 10:12:07 -0700

    Add cast

--- python/mrc/_pymrc/src/utilities/json_values.cpp
+++ python/mrc/_pymrc/src/utilities/json_values.cpp
@@ -255,7 +255,7 @@ nlohmann::json JSONValues::get_json(const std::string& path,
 nlohmann::json JSONValues::stringify(const pybind11::object& obj, const std::string& path)
 {
     AcquireGIL gil;
-    return {py::str(obj)};
+    return {py::str(obj).cast<std::string>()};
 }
 
 JSONValues JSONValues::set_value(const std::string& path, const pybind11::object& value) const
