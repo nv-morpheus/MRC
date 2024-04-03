d841b04d1c28ce0e7fdf51a4719eba25f722139a
David Gardner <dagardner@nvidia.com>
2024-04-03 14:42:09 -0700

    Add python type cast code

--- python/mrc/_pymrc/include/pymrc/utilities/json_values.hpp
+++ python/mrc/_pymrc/include/pymrc/utilities/json_values.hpp
@@ -20,7 +20,7 @@
 #include "pymrc/types.hpp"  // for python_map_t & unserializable_handler_fn_t
 
 #include <nlohmann/json.hpp>
-#include <pybind11/pytypes.h>  // for PYBIND11_EXPORT & pybind11::object
+#include <pybind11/pybind11.h>  // for PYBIND11_EXPORT, pybind11::object, type_caster
 
 #include <cstddef>  // for size_t
 #include <string>
@@ -155,3 +155,58 @@ class PYBIND11_EXPORT JSONValues
 
 #pragma GCC visibility pop
 }  // namespace mrc::pymrc
+
+/****** Pybind11 caster ******************/
+
+// NOLINTNEXTLINE(modernize-concat-nested-namespaces)
+namespace PYBIND11_NAMESPACE {
+namespace detail {
+
+template <>
+struct type_caster<mrc::pymrc::JSONValues>
+{
+  public:
+    /**
+     * This macro establishes a local variable 'value' of type JSONValues
+     */
+    PYBIND11_TYPE_CASTER(mrc::pymrc::JSONValues, _("object"));
+
+    /**
+     * Conversion part 1 (Python->C++): convert a PyObject into JSONValues
+     * instance or return false upon failure. The second argument
+     * indicates whether implicit conversions should be applied.
+     */
+    bool load(handle src, bool convert)
+    {
+        if (!src)
+        {
+            return false;
+        }
+
+        if (src.is_none())
+        {
+            value = mrc::pymrc::JSONValues();
+        }
+        else
+        {
+            value = std::move(mrc::pymrc::JSONValues(pybind11::reinterpret_borrow<pybind11::object>(src)));
+        }
+
+        return true;
+    }
+
+    /**
+     * Conversion part 2 (C++ -> Python): convert a JSONValues instance into
+     * a Python object. The second and third arguments are used to
+     * indicate the return value policy and parent object (for
+     * ``return_value_policy::reference_internal``) and are generally
+     * ignored by implicit casters.
+     */
+    static handle cast(mrc::pymrc::JSONValues src, return_value_policy policy, handle parent)
+    {
+        return src.to_python().release();
+    }
+};
+
+}  // namespace detail
+}  // namespace PYBIND11_NAMESPACE
