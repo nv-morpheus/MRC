0701b132c8659812139e9035c7d080bd01e7a150
David Gardner <96306125+dagardner-nv@users.noreply.github.com>
2024-04-10 15:16:27 -0700

    Consolidate redundant split_string_to_array, split_string_on & split_path methods (#465)
    * All three methods performed nearly identical operations, but were defined in anonymous namespaces and not exposed in a header preventing sharing.
    * Consolidate all three into a new `split_string_to_vector` in `string_utils.hpp`
    
    closes #454
    
    Authors:
      - David Gardner (https://github.com/dagardner-nv)
    
    Approvers:
      - Michael Demoret (https://github.com/mdemoret-nv)
    
    URL: https://github.com/nv-morpheus/MRC/pull/465

--- python/mrc/_pymrc/src/utilities/json_values.cpp
+++ python/mrc/_pymrc/src/utilities/json_values.cpp
@@ -20,9 +20,8 @@
 #include "pymrc/utilities/acquire_gil.hpp"
 #include "pymrc/utils.hpp"
 
-#include "mrc/utils/string_utils.hpp"  // for MRC_CONCAT_STR
+#include "mrc/utils/string_utils.hpp"  // for MRC_CONCAT_STR, split_string_to_array
 
-#include <boost/algorithm/string.hpp>  // for split
 #include <glog/logging.h>
 #include <pybind11/cast.h>
 
@@ -34,13 +33,6 @@
 #include <utility>     // for move
 #include <vector>      // for vector
 
-// We already have <boost/algorithm/string.hpp> included we don't need these others, it is also the only public header
-// with a definition for boost::is_any_of, so even if we replaced string.hpp with these others we would still need to
-// include string.hpp or a detail/ header
-// IWYU pragma: no_include <boost/algorithm/string/classification.hpp>
-// IWYU pragma: no_include <boost/algorithm/string/split.hpp>
-// IWYU pragma: no_include <boost/iterator/iterator_facade.hpp>
-
 namespace py = pybind11;
 using namespace std::string_literals;
 
@@ -48,9 +40,7 @@ namespace {
 
 std::vector<std::string> split_path(const std::string& path)
 {
-    std::vector<std::string> path_parts;
-    boost::split(path_parts, path, boost::is_any_of("/"));
-    return path_parts;
+    return mrc::split_string_to_vector(path, "/"s);
 }
 
 struct PyFoundObject
