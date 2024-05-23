5f0cbeee98151721cbd7ba9eec4c9884dc89cc24
David Gardner <dagardner@nvidia.com>
2024-05-23 13:39:07 -0700

    Update rmm to 24.06

--- ci/conda/recipes/run_conda_build.sh
+++ ci/conda/recipes/run_conda_build.sh
@@ -95,7 +95,7 @@ fi
 # Choose default variants
 if hasArg quick; then
    # For quick build, just do most recent version of rapids
-   CONDA_ARGS_ARRAY+=("--variants" "{rapids_version: 24.02}")
+   CONDA_ARGS_ARRAY+=("--variants" "{rapids_version: 24.06}")
 fi
 
 # And default channels (should match dependencies.yaml)
