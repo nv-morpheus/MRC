7fcf71c08097c2bd690cf22220024ee6b9bad8aa
David Gardner <dagardner@nvidia.com>
2025-03-20 11:41:55 -0700

    Fix tests
    Signed-off-by: David Gardner <dagardner@nvidia.com>

--- cpp/mrc/tests/test_node.cpp
+++ cpp/mrc/tests/test_node.cpp
@@ -896,8 +896,7 @@ TEST_P(PeExceedsTests, PeExceedsResources)
             exec.start();
             exec.join();
         },
-        "A node was destructed which still had dependent "
-        "connections.*",
+        "A node was destructed which still had dependent connections",
         std::runtime_error);
 }
 
