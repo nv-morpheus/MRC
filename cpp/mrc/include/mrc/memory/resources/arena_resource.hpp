9a04ad3c49b3e16c4d0b2e1a5c508a3746d91cd7
David Gardner <96306125+dagardner-nv@users.noreply.github.com>
2026-01-13 14:59:22 -0800

    Update to Rapids 25.06 (#558)
    * Remove the align_up/align_down methods these were thin wrappers around RMM's methods of the same name. These methods were not used outside of the arena code, and due to changes on the RMM side they were creating build errors.
    * Adopt cmake 3.30 (required by rapids-cmake)
    * Resolve version conflicts in the conda build
    
    Authors:
      - David Gardner (https://github.com/dagardner-nv)
    
    Approvers:
      - Will Killian (https://github.com/willkill07)
    
    URL: https://github.com/nv-morpheus/MRC/pull/558

--- cpp/mrc/include/mrc/memory/resources/arena_resource.hpp
+++ cpp/mrc/include/mrc/memory/resources/arena_resource.hpp
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2020-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-FileCopyrightText: Copyright (c) 2020-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -22,6 +22,7 @@
 
 #include <cuda_runtime_api.h>
 #include <glog/logging.h>
+#include <rmm/detail/aligned.hpp>
 #include <rmm/detail/error.hpp>
 #include <rmm/logger.hpp>
 
@@ -135,7 +136,7 @@ class arena_resource final : public adaptor<Upstream>
             return nullptr;
         }
 
-        bytes         = detail::arena::align_up(bytes);
+        bytes         = rmm::align_up(bytes, rmm::CUDA_ALLOCATION_ALIGNMENT);
         auto& arena   = get_thread_arena();
         void* pointer = arena.allocate(bytes);
 
@@ -173,7 +174,7 @@ class arena_resource final : public adaptor<Upstream>
             return;
         }
 
-        bytes = detail::arena::align_up(bytes);
+        bytes = rmm::align_up(bytes, rmm::CUDA_ALLOCATION_ALIGNMENT);
         get_thread_arena().deallocate(ptr, bytes);
     }
 
