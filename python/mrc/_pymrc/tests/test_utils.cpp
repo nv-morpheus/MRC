be807202b1d302c15d9d59fdbaca12e878a71afa
David Gardner <dagardner@nvidia.com>
2026-01-21 10:36:26 -0800

    IWYU fix
    Signed-off-by: David Gardner <dagardner@nvidia.com>

--- python/mrc/_pymrc/tests/test_utils.cpp
+++ python/mrc/_pymrc/tests/test_utils.cpp
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2021-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-FileCopyrightText: Copyright (c) 2021-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,6 +24,7 @@
 #include <glog/logging.h>
 #include <gtest/gtest.h>
 #include <nlohmann/json.hpp>
+#include <moduleobject.h>  // for PyModuleDef
 #include <pybind11/cast.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/pytypes.h>
